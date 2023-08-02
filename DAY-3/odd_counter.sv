87c96987e3011e5ba8f3f993c0d06940f374e0b5
rohith2h2 <110890117+rohith2h2@users.noreply.github.com>
2023-08-01 22:33:11 -0500

    Update odd_counter.sv

--- DAY-3/odd_counter.sv
+++ DAY-3/odd_counter.sv
@@ -6,11 +6,11 @@ module odd_counter (
   logic [7:0] next_count;
 
   always_ff @(posedge clk or posedge reset) begin
-    if(reset)
+    if(reset) //when reset is high 1 is assigned to counter_out
       counter_out <= 8'h1;
-    else 
+    else //when resest is low, next_count is assigned to counter_out
       counter_out <= next_count;
-
+//At rising edge of clk, 2 will be added with counter_out, assigning a odd value to next_count
   assign next_count = counter_out + 8'h2;
   end
 
